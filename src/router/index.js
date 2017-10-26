97e5455e59fab784ccfe7d083c2e1ac3d9026eba
杰 郑 <Doudou105@outlook.com>
2017-10-27 00:24:09 +0800

    add favicon path

--- src/router/index.js
+++ src/router/index.js
@@ -11,36 +11,6 @@ import Frames3 from '@/frame/frame3'
 
 Vue.use(Router)
 
-// export default new Router({
-//  	routes: [
-// 		{	
-//  	    path: '/',
-//  	   	component: Index,
-//  	   	children:[
-//  	   		{
-//  	   			path: '/frame1',
-//  	   			component: Frames1
-//  	   		},
-//  	   		{
-//  	   			path: '/frame2',
-//  	   			component: Frames2
-//  	   		},
-//  	   		{
-//  	   			path: '/frame3',
-//  	   			component: Frames3
-//  	   		},]
-// 		},
-// 		{
-//  	  	path: '/page2',
-//  	  	component:Indepe2
-// 		},
-// 		{
-//  	  	path: '/page3',
-//  	  	component:Indepe3
-// 		},
-//  	]
-// })
-
 const router = new Router({
   	routes: [
 		{	
